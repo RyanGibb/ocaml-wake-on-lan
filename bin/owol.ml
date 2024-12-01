dd5f5e5d29900d7a2e0b884d3105037d2bb00df3
Ryan Gibb <ryan@freumh.org>
2024-12-01 19:10:53 +0000

    update

--- bin/owol.ml
+++ bin/owol.ml
@@ -1,28 +1,42 @@
-let send_wol mac port broadcast =
-  let mac = Macaddr.of_string_exn mac in
-  Eio_posix.run @@ fun env -> Wol_eio.send ~net:env#net ~port ~broadcast mac
+let send_wol mac port address broadcast =
+  Eio_posix.run @@ fun env ->
+  Wol_eio.send ~net:env#net ~port ~address ~broadcast mac
 
 let () =
   let open Cmdliner in
   let mac_address =
     let doc = "The MAC address to send the magic packet to." in
-    Arg.(required & pos 0 (some string) None & info [] ~docv:"MAC_ADDRESS" ~doc)
+    Arg.(
+      required
+      & pos 0 (some (Cmdliner.Arg.conv (Macaddr.of_string, Macaddr.pp))) None
+      & info [] ~docv:"MAC_ADDRESS" ~doc)
   in
   let port =
     let doc = "Port to send the packet too (default 9)." in
     Arg.(value & opt int 9 & info [ "p"; "port" ] ~docv:"PORT" ~doc)
   in
-  let broadcast =
+  let address =
     let doc =
       "Address to send the packet too (default broadcast to 255.255.255.255)."
     in
     Arg.(
       value
-      & opt string "255.255.255.255"
+      & opt
+          (Cmdliner.Arg.conv (Ipaddr.V4.of_string, Ipaddr.V4.pp))
+          (Ipaddr.V4.of_string_exn "255.255.255.255")
       & info [ "a"; "address" ] ~docv:"ADDRESS" ~doc)
   in
+  let broadcast =
+    let doc =
+      "Whether to set the SO_BROADCAST socket option to enable broadcasting \
+       (default true)."
+    in
+    Arg.(value & opt bool true & info [ "a"; "address" ] ~docv:"ADDRESS" ~doc)
+  in
   let cmd =
-    let term = Term.(const send_wol $ mac_address $ port $ broadcast) in
+    let term =
+      Term.(const send_wol $ mac_address $ port $ address $ broadcast)
+    in
     let doc = "Send a Wake-on-LAN magic packet with a specified MAC address." in
     let info = Cmd.info "wol" ~doc in
     Cmd.v info term
--- src/eio/wol_eio.ml
+++ src/eio/wol_eio.ml
@@ -1,9 +1,7 @@
-let send ~net ?(port = 9) ?(broadcast = "255.255.255.255") mac_str =
+let send ~net ?(port = 9) ?(address = Ipaddr.V4.of_string_exn "255.255.255.255")
+    ?(broadcast = true) mac_str =
   Eio.Switch.run @@ fun sw ->
-  let addr =
-    Ipaddr.V4.of_string_exn broadcast
-    |> Ipaddr.V4.to_octets |> Eio.Net.Ipaddr.of_raw
-  in
+  let addr = address |> Ipaddr.V4.to_octets |> Eio.Net.Ipaddr.of_raw in
   let sock =
     let proto =
       Eio.Net.Ipaddr.fold ~v4:(fun _v4 -> `UdpV4) ~v6:(fun _v6 -> `UdpV6) addr
@@ -11,6 +9,8 @@ let send ~net ?(port = 9) ?(broadcast = "255.255.255.255") mac_str =
     Eio.Net.datagram_socket ~sw net proto
   in
   let fd = Option.get (Eio_unix.Resource.fd_opt sock) in
-  Eio_unix.Fd.use_exn "broadcast" fd (fun fd -> Unix.setsockopt fd Unix.SO_BROADCAST true);
+  if broadcast then
+    Eio_unix.Fd.use_exn "broadcast" fd (fun fd ->
+        Unix.setsockopt fd Unix.SO_BROADCAST true);
   let packet = Wol.magic_packet mac_str in
   Eio.Net.send sock ~dst:(`Udp (addr, port)) [ packet ]
